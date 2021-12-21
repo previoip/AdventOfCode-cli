6bbe936161e250191889745b8b19d71aedc19a78
previoip <previo.prakasa@gmail.com>
2021-12-21 18:35:03 +0700

    rev: _genReadme

--- _genReadme.py
+++ _genReadme.py
@@ -29,19 +29,38 @@ def main():
             cols = int(len(compDict[year])/10)+1
             table += ' '.join(["| Day  | Stars |" for _ in range(cols)])
             table += "\n"
-            table += '-'.join(["| :-:  | :---- |" for _ in range(cols)])
+            table += '-'.join(["| :-:  | :---: |" for _ in range(cols)])
             table += "\n"
             for i in range(1, 11):
                 # table += "| %s  | %s%s |\n" % (day, ' *' if compDict[year][str(day)]['1'] else '  ', '* ' if compDict[year][str(day)]['2'] else '  ')
                 row = []
                 for j in range(cols):
                     index = i + (10*j)
-                    try:
-                        (part1, part2) = (compDict[year][str(index)]['1'], compDict[year][str(index)]['2'])
-                        row.append( "| %s  | %s%s |" % (index, ' *' if part1 else '  ', '* ' if part2 else '  '))
-                    except KeyError:
-                        row.append( "|     |     |" )
-                        pass
+
+                    if index > 25:
+                        row.append( "|      |       |" )
+                    else:
+                        temp = '|  %s%s  |  ' % (index, '' if index>=10 else ' ')
+                        try:
+                            part1 = compDict[year][str(index)]['1']
+                            temp += '%s' % ('*' if part1 else ' ')
+                        except KeyError:
+                            temp += ' '
+
+                        try:
+                            part2 = compDict[year][str(index)]['2']
+                            temp += '%s' % ('*' if part2 else ' ')
+                        except KeyError:
+                            temp += ' '
+                        temp += '   |'
+
+                        row.append(temp)
+                    # try:
+                    #     (part1, part2) = (compDict[year][str(index)]['1'], compDict[year][str(index)]['2'])
+                    #     row.append( "| %s  | %s%s |" % (index, ' *' if part1 else '  ', '* ' if part2 else '  '))
+                    # except KeyError:
+                    #     row.append( "|     |     |" )
+                    #     pass
                 table += ' '.join(row)
                 table += "\n"
             table += "\n"
