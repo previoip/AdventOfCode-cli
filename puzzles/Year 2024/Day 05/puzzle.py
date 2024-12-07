e9c52ad44797a93b6f6dff7dbaa30382a4fc5806
Previo Prakasa <previo.prakasa@gmail.com>
2024-12-07 21:33:55 +0700

    y2024 d5

--- puzzles/Year 2024/Day 05/puzzle.py
+++ puzzles/Year 2024/Day 05/puzzle.py
@@ -1,6 +1,7 @@
 import typing as t
 from io import IOBase
 from src.aoc.base_class import AOCBaseClass, AOCRunAsEnum
+from itertools import permutations
 
 class AOC(AOCBaseClass):
 
@@ -19,9 +20,11 @@ class AOC(AOCBaseClass):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.eval_path_part_1 = 'input_p1.txt'
-    self.eval_path_part_2 = 'input_p2.txt'
+    self.eval_path_part_2 = 'input_p1.txt'
     self.test_path_part_1 = 'test_p1.txt'
     self.test_path_part_2 = 'test_p2.txt'
+    self.ordering_ruleset_afters = None
+    self.ordering_ruleset_priors = None
 
   def process_test_answer(self, b: bytes) -> t.Any:
     return int(b.decode(self.default_encoding))
@@ -39,6 +42,10 @@ class AOC(AOCBaseClass):
         ordering_rules.append(tuple(map(int, line.split('|'))))
       else:
         update_sets.append(tuple(map(int, line.split(','))))
+
+    self.ordering_ruleset_afters = self.pairs_to_tree(ordering_rules)
+    self.ordering_ruleset_priors = self.pairs_to_tree([(i[1], i[0]) for i in ordering_rules])
+
     return ordering_rules, update_sets
 
   @staticmethod
@@ -52,27 +59,65 @@ class AOC(AOCBaseClass):
 
   @staticmethod
   def ck_left_in_right(left, right):
-    for i in left:
+    for n, i in enumerate(left):
       if i in right:
-        return True
-    return False
+        return (n, right.index(i)), True
+    return (None, None), False
+
+  def validate_pages(self, page_set):
+    empty = list()
+    for i, page in enumerate(page_set):
+      l_set = page_set[:i]
+      r_set = page_set[i+1:]
+
+      rule_after = self.ordering_ruleset_afters.get(page, empty)
+      rule_prior = self.ordering_ruleset_priors.get(page, empty)
+      (r_index, rule_index), test = self.ck_left_in_right(r_set, rule_prior)
+      if test:
+        return False, ('prior', r_index+i, rule_index, rule_after, rule_prior)
+      (l_index, rule_index), test = self.ck_left_in_right(l_set, rule_after)
+      if test:
+        return False, ('after', l_index, rule_index, rule_after, rule_prior)
+    return True, (None, None, None, None)
 
   def solution_part_1(self, parsed_input) -> t.Any:
-    ordering_rules, update_sets = parsed_input
-    empty = list()
-    ruleset_afters = self.pairs_to_tree(ordering_rules)
-    ruleset_priors = self.pairs_to_tree([(i[1], i[0]) for i in ordering_rules])
+    _, update_sets = parsed_input
     sums = 0
-    for p, update_set in enumerate(update_sets):
-      print(update_set)
-      for i, page in enumerate(update_set):
-        print(page, ruleset_afters.get(page), ruleset_priors.get(page))
-        rule_after = ruleset_afters.get(page, empty)
-        rule_prior = ruleset_priors.get(page, empty)
-        l = update_set[:i]
-        r = update_set[i+1:]
+    for p, page_set in enumerate(update_sets):
+      print(page_set)
+      test, _ = self.validate_pages(page_set)
+      if test:
+        sums += page_set[len(page_set) // 2]
     return sums
 
   def solution_part_2(self, parsed_input) -> t.Any:
-    raise NotImplementedError('puzzle part 2 is not yet implemented')
+    _, update_sets = parsed_input
+    sums = 0
+    for p, page_set in enumerate(update_sets):
+      page_set = list(page_set)
+      page_length = len(page_set)
+      print()
+      print(page_set)
+      test, info = self.validate_pages(page_set)
+      if not test:
+        for _ in range(10_000):
+          test, info = self.validate_pages(page_set)
+          if test: break
+          which, page_index, rule_index, _, _ = info
+          page_invalid = page_set[page_index]
+          if which == 'prior':
+            rule = self.ordering_ruleset_priors[page_invalid]
+            for i in range(page_index+1, page_length):
+              if page_set[i] in rule:
+                page_set[page_index] = page_set[i]
+                page_set[i] = page_invalid
+                break
+            else:
+              print('waoawafgahffaaghhgao')
+          else:
+            print('!!!!!!!!!!!!!!!!!!')
+          print('<<', page_set)
+        sums += page_set[len(page_set) // 2]
+    return sums
+
     return parsed_input
